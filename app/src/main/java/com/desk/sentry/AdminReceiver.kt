e3b63c8a14b1d99176b96e1c807c85e91297412e
subodhcoc222-dev <subodhcoc222@gmail.com>
2026-08-26 10:28:11 +0530

    Create AdminReceiver.kt

---
+++ app/src/main/java/com/desk/sentry/AdminReceiver.kt
@@ -0,0 +1,13 @@
+package com.desk.sentry
+
+import android.app.admin.DeviceAdminReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class AdminReceiver : DeviceAdminReceiver() {
+    override fun onEnabled(context: Context, intent: Intent) {
+        super.onEnabled(context, intent)
+        Toast.makeText(context, "Desk Sentry Protection Activated!", Toast.LENGTH_SHORT).show()
+    }
+}
